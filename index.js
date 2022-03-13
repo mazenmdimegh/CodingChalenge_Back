7beb78818f7f20e756db2112d7d055d220526024
mazenmdimegh <mazenmdimagh@gmail.com>
2022-03-13 09:47:47 +0100

    Crud fix

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ require('./db/connect')
 const express = require('express');
 const appDebug = require('debug')('app:debug');
 const {Conversion } = require('./models/conversion');
+const router= require('./routers/conversions');
+const bodyParser    = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/api/conversions',router);
 
 let conversion = new Conversion({
     from : "EUR",
@@ -13,7 +20,7 @@ let conversion = new Conversion({
     amount : 100,
     result : 1000 
 });
-console.log(conversion.save());
+//console.log(conversion.save());
 
 
 app.listen(port,()=> appDebug(`server on ${port}`))
--- routers/conversions.js
+++ routers/conversions.js
@@ -6,6 +6,13 @@ const {Conversion,validation_Conversion}=require("../models/conversion")
 router.get('',async(req,res)=>{
     res.send(await Conversion.find());
 })
+router.get('/:id',async(req,res)=>{
+    try {
+    res.send(await Conversion.findById(req.params.id));
+    } catch (error) {
+    res.status(400).send('Error finding conversion :'+error.message);
+    }
+})
 
 router.post('',async(req,res)=>{
     let result= validation_Conversion.validate(req.body);
@@ -14,3 +21,21 @@ router.post('',async(req,res)=>{
     let conversion = new Conversion(req.body);
     res.send(await conversion.save());
 })
+router.put('',async(req,res)=>{
+    let result= validation_Conversion.validate(req.body);
+    if(!result)
+        res.status(400).send("wrong format")
+    let conversion = new Conversion(req.body);
+    res.send(await conversion.save());
+})
+router.delete('/:id',async(req,res)=>{
+    try {
+        let conv = await Conversion.findByIdAndRemove(req.params.id);
+        if(!conv)
+            return res.status(404).send('conversion with id is not found');
+        res.send(conv);
+    } catch (error) {
+        res.status(400).send('Error Deleting conversion :',error.message);
+    }
+})
+module.exports = router
